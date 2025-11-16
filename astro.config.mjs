9c8fc4b735626e856c73d42a26ab390bc4a011ec
paaveldev <kachitotv@gmail.com>
2025-11-16 12:12:39 +0200

    astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://kyxel.example',
+  site: 'https://paaveldev.github.io/kyxel-security-frontend/',
+  base: '/kyxel-security-frontend/',
   integrations: [],
 });
