7ea02e2cb39cd98721fa2a6fe6f30a56f6865193
Pavel Todica <kachitotv@gmail.com>
2025-11-23 17:36:04 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://paaveldev.github.io/kyxel-security-frontend/',
-  base: '/kyxel-security-frontend/',
+  site: 'https://kyxelsecurity.com/',
+  base: '/',
   integrations: [],
 });
